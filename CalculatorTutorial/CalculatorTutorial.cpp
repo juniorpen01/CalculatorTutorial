d3bd118b67718c9f04e5a8e55a35970056e4ef22
juniorpen01 <01juniorpen@gmail.com>
2025-12-25 14:51:09 +0800

    Implement calculator repl with error handling

--- CalculatorTutorial/CalculatorTutorial.cpp
+++ CalculatorTutorial/CalculatorTutorial.cpp
@@ -1,16 +1,33 @@
 // CalculatorTutorial.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+#include <iostream> // <> looks in system directories; "" looks in project directories, what <> also does later
+
+#include "Calculator.h" // i wanna separate std, third party, and project level
+
+// no using namespace std cuz it sucks; supposed to bring namespace std into scope
 
 int main()
 {
 	std::cout << R"(Calculator Console Application
 
 Please enter an operation to perform. Format: a+b | a-b | a*b | a/b
-)";
+)"; // << some weird ahh operator but ye usually for out; in this case stdout; raw string cuz it look better imo; couldve chosen either \n or endl, but \n generally better practice for it doesnt instantly flush buffer
+	// ends in semicolons btw
+	// main is linker entry point i think
 
-
+	while (true) {
+		double x, y; char oper; Calculator c; // idk why decl all on same line
+		if (!(std::cin >> x >> oper >> y)) { // apparenly cin has a bool operator; but ye cin can go fail state and not take any input cuz of it
+			std::cin.clear(); // clears state
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n'); // numeric limit max of stream size; ignores that many until \n
+			std::cout << "Invalid input\n";
+			continue;
+		}
+		double result =
+			c.Calculate(x, oper, y);
+		std::cout << "Result is: " << result << '\n';
+	}
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
